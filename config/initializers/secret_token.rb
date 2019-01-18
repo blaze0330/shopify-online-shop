e10b300b79242b548b0efe96859f2b8948e33ad0
blaze0330 <jamesbakker0330@gmail.com>
2019-01-18 18:26:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnlineMarketplace::Application.config.secret_key_base = '6d07e20e1b883233e45b3c7811c3924fafef0ff77e3a1b3cc0a107ed8b378d8d6646d4b24afd4a939f69a3da727c9b7c66b24e05ab97bc243eda13a4ece67a97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnlineMarketplace::Application.config.session_store :cookie_store, key: '_online_marketplace_session'
